39dc951c672e9b48ba06856479e6b077f7922e51
cmstar <cosmo_sei@hotmail.com>
2022-05-15 20:07:16 +0800

    Fix a variable error in the build script.

--- pack.bat
+++ pack.bat
@@ -2,4 +2,4 @@ SET cur_dir=%~dp0
 SET output=%cur_dir%.build
 
 SET build_cmd=dotnet pack -c Release --output %output%
-%build_cmd% %curdir%src\cmstar.RapidReflection
+%build_cmd% %cur_dir%src\cmstar.RapidReflection
